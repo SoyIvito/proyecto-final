1f1183677a9813d4d7d347d50b77d8f6ab4b150d
SoyIvito <ivannovotny6@gmail.com>
2025-07-18 11:31:07 -0300

    se agregaron 7 nuevas recetas

---
+++ recetas.py
@@ -0,0 +1,50 @@
+#RECETA MILANESA DE POLLO: https://cookpad.com/ar/r/24591349
+def r_milanesa_pollo():
+    print("1. Batir los huevos y despues agregar los condimentos uno por uno")
+    print("2. Y a las pechugas filetear y agregarle sal a cada una de \n las pechugas y después agregarlas a la mezcla con huevo y batir todo")
+    print("3. Despues poner en una fuente todo el pan rallado e ir empenando las milanesas")
+    print("4. Luego fritar o cocinar en el horno (Horno 20/25 mins) (Fritura: 15 mins)")
+
+#RECETA MILENESA DE CARNE: https://cookpad.com/ar/recetas/22660552?ref=search&search_term=milanesa+de+carne
+def r_milanesa_carne():
+    print("1. Batimos los huevos con la leche, echamos mostaza, perejil,\n ajo picado y reservamos")
+    print("2. Salar la carne a gusto e incorporar a la mezcla de huevos y marinar\n (si se quiere que agarre sabor, dejar marinar durante 3 horas)")
+    print("3. Pasar por pan rallado, si sobra mucho pan rallado, se le\n puede hacer un apanado doble")
+
+#RECETA PIZZA: https://cookpad.com/ar/recetas/24903782?ref=search&search_term=pizza
+def r_pizza():
+    print("1. En un recipiente ponemos la harina, formamos una corona e\n incorporar sal en los bordes y en el centro\n el agua con el fermentado y las cdas de aceite")
+    print("2. Una vez amasado, dejar reposar unos 30 mins hasta que duplique su volumen")
+    print("3. Armar bollitos y armar pizzas (tamaño a gusto)")
+
+#PURE DE PAPAS: https://cookpad.com/ar/recetas/23909118?ref=search&search_term=pure+de+papas
+def r_pure():
+    print("1. Lavamos y pelamos las papas. Cortamos en cubos grandes, colocamos en una olla con abundante agua con sal.\n Llevamos a hervor hasta que estén tiernas, unos 20 minutos dependiendo del tamaño y la cantidad.")
+    print("2. Mientras picamos finamente el ajo y el perejil. Reservamos")
+    print("3. Colocamos en caliente, pisamos con ayuda de un pisaáás hasta obtener un pure liso.\n Añadimos la manteca y la pimienta. Rectificamos sazon si lo necesitara.\n Mezclamos y añadimos el ajo y el perejil picados. Integrar bien")
+
+#GUISO DE LENTEJAS: https://cookpad.com/ar/recetas/24901620?ref=search&search_term=guiso
+def r_guiso():
+    print("1. En una cacerola poner las lentejas (previas en remojo toda la noche, las\n papas cortadas en cubos y las zanahorias en rodajas. Agregar\n agua hasta tapar los ingredientes. Tambien poner el caldo)")
+    print("2. Mientras tanto en otra sarten, hacer el sofrito: panceta, cebolla, puerro y tomate")
+    print("3. Cuando la cebolla y el puerro esten transparentes, agregamos el sofrito a la cacerola\n con las lentejas. Cocinamos 10 mins mas hasta que esten tiernas")
+    print("4. Apagamos el fuego, dejamos reposar unos minutos y listo!")
+
+#TORTILLA DE PAPAS: https://cookpad.com/ar/recetas/24807813?ref=search&search_term=tortilla+de+papas
+def r_tortilla_papas():
+    print("1. Despues de lavar y pelar las papas, las cortamos en cubos (o en la forma de preferencia)")
+    print("2. Freir las papas y reservar")
+    print("3. Colocamos en un bowl las papas fritas y le agregamos oregano y sal a gusto.\n Mezclamos para que agarre sabos. Posteriormente le\n agregamos los 6 huevos (varian segun la cantidad y necesidad de papas)")
+    print("4. Colocamos la mezcla en una sarten caliente con aceita y la damos vuelta y\n vuelta hasta que este dorada. Retiramos del fuego")
+
+#EMPANADAS DE CARNE: https://cookpad.com/ar/recetas/24807813?ref=search&search_term=tortilla+de+papas
+def r_empanadas():
+    print("1. Picar bien chiquito la cebolla, los pimientos")
+    print("2. Para el relleno, sofreir toda la cebolla picada en el aceite bien caliente.\n Cuando comience a dorarse, añadir el pimenton y mezclar por\n 5 minutos.")
+    print("3. Cuando la cebolla cristalice, agregar las especias: el comino, pimenton rojo molido\n y la pimienta blanca")
+    print("4. Cocinar las papas cortadas en cubos pequeños, en agua hirviendo por solo 5 mins")
+    print("5. Revuelto todo, agregar la carne bien cortada. Mezclar todo y dejar que la carne se\n cocine a fuego bajo por 15 mins. Agregar las papas y continuar. Retirar\n del fuegominutos antes de terminar la coccion y dejar enfriar.\n Mientras precalentar el horno a 250°C")
+    print("6. Agregar la cebolla de verdeo picada, el perejil y los huevos duros picados justo\n antes de armar las empanadas")
+    print("7. Cocinar en el horno a 250° durante unos 12 minutos o freir en abundante aceite")
+
+recetas = {"milanesa de pollo", "milanesa de carne", "pizza", "pure de papas", "guiso", "tortilla de papas", "empanadas"}
